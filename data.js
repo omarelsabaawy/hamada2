0e5b60f2753eac6798de4c265fdce58fd702ac79
omarelsabaawy <omarelsabaawy77@gmail.com>
2022-11-30 13:11:52 +0200

    first edition

---
+++ data.js
@@ -0,0 +1,50 @@
+const data = {
+    products: [
+        {
+            name: "Jumanji Game for ps4",
+            slug: "Jumanji-Game-for-ps4",
+            category: "Playstation games",
+            image: "/img/Jumanji.jpg",
+            price: 50,
+            countInStock: 1,
+            owner: "George hu",
+            swap: true,
+            buy: false
+        },
+        {
+            name: "Mortal Kombat 11 Game for ps4",
+            slug: "Mortal-Kombat-11-Game-for-ps4",
+            category: "Playstation games",
+            image: "/img/MortalKombat.jpeg",
+            price: 80,
+            countInStock: 1,
+            owner: "Guy mike",
+            swap: true,
+            buy: false
+        },
+        {
+            name: "Mortal Kombat 11 Game for ps5",
+            slug: "Mortal-Kombat-11-Game-for-ps5",
+            category: "Playstation games",
+            image: "/img/MortalKombatps5.jpg",
+            price: 100,
+            countInStock: 1,
+            owner: "Michele william",
+            swap: true,
+            buy: false
+        },
+        {
+            name: "Stranger Things 3 Game for ps4",
+            slug: "Stranger-Things-3-Game-for-ps4",
+            category: "Playstation games",
+            image: "/img/StrangerThings3.jpg",
+            price: 40,
+            countInStock: 1,
+            owner: "Louis james",
+            swap: true,
+            buy: false
+        }
+    ]
+}
+
+export default data;
